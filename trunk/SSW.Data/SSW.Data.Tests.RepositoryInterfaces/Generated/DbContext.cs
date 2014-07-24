71082410294823fca35425e07bfafde10f188c60
Igor Goldobin <igorgoldobin@ssw.com.au>
2014-07-24 17:03:28 +1000

    Fixed config file for base repositories

---
+++ trunk/SSW.Data/SSW.Data.Tests.RepositoryInterfaces/Generated/DbContext.cs
@@ -0,0 +1,20 @@
+﻿ 
+
+namespace SSW.Data
+{
+	using System.Data.Entity;
+	using SSW.Data.Tests.DomainModel.Entities;
+
+	using SSW.Data.Entities;
+
+	public partial class TestDbContext
+	{
+		public IDbSet<Entity2> Entity2s { get; set; }
+
+		public IDbSet<Entity3> Entity3s { get; set; }
+
+		public IDbSet<Entity4> Entity4s { get; set; }
+
+	}
+}
+
