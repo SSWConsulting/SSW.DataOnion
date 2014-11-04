b506996dd3082250af3e0b27e4a6856bbfae7962
Brendan Richards <brendan@nobadthing.com>
2014-11-04 16:58:25 +1000

    added missing files.

---
+++ trunk/SSW.Data/SSW.Data.Tests.Integration/DependencyResolution/IOC.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+
+namespace SSW.Data.Tests.Integration.DependencyResolution
+{
+    public class IOC
+    {
+
+        public static IContainer Configure()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterModule(new SSW.Data.Tests.Integration.DependencyResolution.TestModule()
+            {
+                //ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["CmsEntities"].ConnectionString
+            });
+
+
+            var container = builder.Build();
+
+            return container;
+        }
+
+    }
+}
