5dce96ba1de97ea3506315fc1e3e12b190927733
Igor Goldobin <igorgoldobin@ssw.com.au>
2014-11-05 09:30:47 +1000

    Merging commits

---
+++ trunk/SSW.Data/SSWDataTemplate/ProjectTemplates/SSWProjects/DataTemplate/Data/DbContextFactory.cs
@@ -0,0 +1,29 @@
+namespace SSW.Data1.Data
+{
+    using System.Data.Entity;
+    using System.Data.Entity.Infrastructure;
+
+    public class YourDbContextFactory : IDbContextFactory<YourDbContext>
+    {
+        private bool hasSetInitializer;
+
+        private IDatabaseInitializer<YourDbContext> dbInitializer;
+
+        public YourDbContextFactory(IDatabaseInitializer<YourDbContext> dbInitializer)
+        {
+            this.dbInitializer = dbInitializer;
+        }
+
+        public YourDbContext Create()
+        {
+            if (!this.hasSetInitializer)
+            {
+                Database.SetInitializer(this.dbInitializer);
+
+                this.hasSetInitializer = true;
+            }
+
+            return new YourDbContext();
+        }
+    }
+}
---
+++ trunk/SSW.Data/SSWDataTemplate/ProjectTemplates/SSWProjects/DataTemplate/Data/Generated/Repositories1.cs
@@ -0,0 +1,25 @@
+﻿ 
+
+using SSW.Data1.DomainModel.Entities;
+using SSW.Data1.Interfaces.Repositories;
+
+using SSW.Data.EF;
+
+namespace SSW.Data1.Data
+{
+	public partial class Entity2Repository : BaseRepository<Entity2>, IEntity2Repository
+	{
+		public Entity2Repository(IDbContextManager contextmanager)
+			: base(contextmanager)
+		{
+		}
+	}
+	public partial class Entity1Repository : BaseRepository<Entity1>, IEntity1Repository
+	{
+		public Entity1Repository(IDbContextManager contextmanager)
+			: base(contextmanager)
+		{
+		}
+	}
+}
+
