ef0a285603745e0f6d539bc87773da4108ca2f1a
Igor Goldobin <igorgoldobin@ssw.com.au>
2014-11-06 15:04:51 +1000

    Updated scripts to autoincrement version numbers

--- trunk/SSW.Data/SSW.Data.Entities/CreatePackageAndPublish.ps1
+++ trunk/SSW.Data/SSW.Data.Entities/CreatePackageAndPublish.ps1
@@ -1,15 +1,9 @@
-﻿$0 = $myInvocation.MyCommand.Definition
+﻿.\CreatePackage.ps1
+
+$0 = $myInvocation.MyCommand.Definition
 $dp0 = [System.IO.Path]::GetDirectoryName($0)
 
 cd $dp0
-
-echo $dp0
-
-echo "Cleaning up old packages..."
-del *.nupkg
-
-echo "Building and packaging new version of package..."
-nuget pack SSW.Data.Entities.csproj -Prop Configuration=Release -IncludeReferencedProjects -Build
 
 echo "Pushing new package to server..."
 $content = [IO.File]::ReadAllText($dp0 + "\..\api.txt")
