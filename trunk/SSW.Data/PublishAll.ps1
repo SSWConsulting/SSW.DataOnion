0a382d2c0192b4f07616193df9113507f01c419a
Igor Goldobin <igorgoldobin@ssw.com.au>
2014-11-06 10:55:06 +1000

    Added script to publish all packages

---
+++ trunk/SSW.Data/PublishAll.ps1
@@ -0,0 +1,23 @@
+echo "Publish SSW.Data.Entities"
+cd SSW.Data.Entities
+.\CreatePackageAndPublish.ps1
+cd..
+
+echo "Publish SSW.Data.EF"
+cd SSW.Data.EF
+.\CreatePackageAndPublish.ps1
+cd..
+
+echo "Publish SSW.Data.DbContext.Generator"
+cd SSW.Data.DbContext.Generator
+.\CreatePackageAndPublish.ps1
+cd..
+
+echo "Publish SSW.Data.RepositoryInterfaces.Generator"
+cd SSW.Data.RepositoryInterfaces.Generator
+.\CreatePackageAndPublish.ps1
+cd..
+
+echo "Publish SSW.Data.Repositories.Generator"
+cd SSW.Data.Repositories.Generator
+.\CreatePackageAndPublish.ps1
